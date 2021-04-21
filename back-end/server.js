24b8338d41145e66c4eeae08ce678305f15d97a6
Benjamin Shafer <contrariwiseowl@gmail.com>
2021-04-20 20:05:53 -0600

    adding server

---
+++ back-end/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// connect to the mongodb database
+mongoose.connect('mongodb://localhost:27017/iheartpoetry', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+    name: 'session',
+    keys: [
+        'secretValue'
+    ],
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    }
+}));
+
+// import modules and setup API paths
